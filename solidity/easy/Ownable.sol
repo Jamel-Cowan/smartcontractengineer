a4f2be9bac3ce258a10b73391446d16734e0a4d1
Block Noob <98855710+Jamel-Cowan@users.noreply.github.com>
2024-01-25 18:50:53 -0500

    easy update

--- solidity/easy/Ownable.sol
+++ solidity/easy/Ownable.sol
@@ -13,3 +13,21 @@
 
 
 // Solution
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Ownable {
+    address public owner;
+    
+    constructor() {
+        owner = msg.sender;
+    }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not owner" );
+        _;
+    }
+    function setOwner(address newOwner) external onlyOwner {
+        require( newOwner != address(0), "new owner = zero address" );
+        owner = newOwner;
+    }
+}
