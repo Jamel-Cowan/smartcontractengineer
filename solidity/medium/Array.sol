b8c4e4eae0d060890f7fb4fe49353bb1d5c17066
Block Noob <98855710+Jamel-Cowan@users.noreply.github.com>
2024-01-28 20:28:33 -0500

    medium update

---
+++ solidity/medium/Array.sol
@@ -0,0 +1,15 @@
+// Task
+// 1. Write function get(uint256 i) that will return the array element of arr at index i.
+
+// This function is external and view.
+
+// 2. Write external function push, 
+// it takes in a single input x of type uint256 and appends x to the end of the array arr.
+
+// 3. Write external function remove(uint256 i) which will delete element at i from the array arr.
+
+// 4.Write function getLength which will return the current length of the array arr.
+
+// This function must be external and view.
+
+// Solution
